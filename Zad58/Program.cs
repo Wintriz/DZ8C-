c82a817a3f8dcb10f8b2bba9f477eab4548e5dea
Andrey <rf5800@mail.ru>
2022-12-30 00:44:27 +0300

    dz8

---
+++ Zad58/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 58: Задайте два двумерных массива (от 0 до 10). Напишите программу, которая будет находить произведение двух массивов (поэлементное).
+// Например, даны 2 массива:
+// 2 4
+// 3 2
+
+// 3 4
+// 3 3
+// Результирующая матрица будет:
+// 6 16
+// 9 6
+
+
+
+int[,] array = new int[2, 2];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+    }
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + "  ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+int[,] array1 = new int[2,2];
+for (int i = 0; i < array1.GetLength(0); i++)
+{
+    for (int j = 0; j < array1.GetLength(1); j++)
+    {
+        array1[i, j] = new Random().Next(1, 10);
+    }
+}
+for (int i = 0; i < array1.GetLength(0); i++)
+{
+    for (int j = 0; j < array1.GetLength(1); j++)
+    {
+        Console.Write(array1[i, j] + "  ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+int[,] arraySum = new int[2,2];
+for (int i = 0; i < arraySum.GetLength(0); i++)
+{
+    for (int j = 0; j < arraySum.GetLength(1); j++)
+    {
+        
+        arraySum[i, j] = 0;
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            arraySum[i,j] = array[i, j] * array1[i, j];
+        }
+                 Console.Write(arraySum[i,j] + "  ");
+    }
+    Console.WriteLine();
+}
+
---
+++ Zad60/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] array = new int[2, 2, 2];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(2); k++)
+        {
+            array[i, j, k] = new Random().Next(10, 100);
+        }
+    }
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write($"X({i}) Y({j}) ");
+        for (int k = 0; k < array.GetLength(2); k++)
+        {
+            Console.Write($"Z({k})={array[i, j, k]}; ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
